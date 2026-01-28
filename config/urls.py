b3d6ca402523f364411022ac18740fd3d9d1c8f0
chrry03 <ocr925@dgu.ac.kr>
2026-01-28 17:25:50 +0900

    logs/models.py 이미지 관련 하여 config/settings.py, urls.py 수정

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings             # [추가] 설정 가져오기
+from django.conf.urls.static import static   # [추가] 정적 파일 서빙 함수
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/users/', include('users.urls')),
+    path('api/ingredients/', include('ingredients.urls')),
+    path('api/recipes/', include('recipes.urls')),
+    path('api/logs/', include('logs.urls')),
 ]
+
+# 미디어 파일 서빙 설정 (개발 모드용): 개발 모드일 때만 미디어 파일 서빙 가능하도록 설정
+# settings.py의 DEBUG=TRUE일때(개발중일때)만 내 컴퓨터의 media폴더의 이미지를 띄울 수 있게 함
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
