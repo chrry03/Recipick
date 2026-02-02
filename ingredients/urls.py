75fd4f970f11bc35431b411e351cbb750bd2c09f
YanggalleDolmery <gamaen9183@gmail.com>
2026-02-02 21:45:22 +0900

    merge 오류, style.css,base_navbar 변경

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -1,23 +1,22 @@
+# ingredients/urls.py
+
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import IngredientViewSet, IngredientCategoryViewSet, UserIngredientViewSet
+# from rest_framework.routers import DefaultRouter  <-- 주석 처리 (#)
+# from .views import IngredientViewSet, IngredientCategoryViewSet, UserIngredientViewSet <-- 주석 처리 (#)
 from . import views
 
+# router = DefaultRouter() <-- 주석 처리 (#)
+# router.register('ingredients', IngredientViewSet, basename='ingredient') <-- 주석 처리 (#)
+# router.register('ingredients/categories', IngredientCategoryViewSet, basename='ingredient-category') <-- 주석 처리 (#)
+# router.register('user-ingredients', UserIngredientViewSet, basename='user-ingredient') <-- 주석 처리 (#)
 
-router = DefaultRouter()
-router.register('ingredients', IngredientViewSet, basename='ingredient')
-router.register('ingredients/categories', IngredientCategoryViewSet, basename='ingredient-category')
-router.register('user-ingredients', UserIngredientViewSet, basename='user-ingredient')
+app_name = 'ingredients'
 
 urlpatterns = [
     # 1. 화면 페이지 (Template View)
-    # 최종 URL: http://127.0.0.1:8000/ingredients/fridge/
     path('fridge/', views.my_fridge_view, name='my_fridge'),
-    
-    # 최종 URL: http://127.0.0.1:8000/ingredients/add/
     path('add/', views.add_ingredient_view, name='add_ingredient'),
     
     # 2. API 라우터 (API View)
-    # 최종 URL: http://127.0.0.1:8000/ingredients/ingredients/ (REST API)
-    path('', include(router.urls)), 
+    # path('', include(router.urls)), <-- 주석 처리 (#)
 ]
