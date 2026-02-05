a995652660b03f4e4e58ed8010ec598a72da523a
chrry03 <ocr925@dgu.ac.kr>
2026-02-06 04:10:23 +0900

    config/urls.py url 수정함 메인페이지를 홈으로 만들고 users앱은 앱별 유알엘과 규칙동일하게 바꿈.

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from users.views import main_view # 메인 뷰만 여기서 직접 임포트
+
 # Swagger 설정
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,17 +21,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # 어드민 페이지
+    
+    # 1. 사이트 대문 (http://127.0.0.1:8000/)
+    path('', main_view, name='root'),
+
+    # 2. 각 앱별 URL (규칙 통일)
+    path('users/', include('users.urls')),              # users 앱
+    path('ingredients/', include('ingredients.urls')),  # ingredients 앱
+    path('recipes/', include('recipes.urls')),          # recipes 앱
+    path('logs/', include('logs.urls')),                # logs 앱
     
     # Swagger URLs
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    
-    # 앱별 URL (각각 한 번씩만 include)
-    path('', include('users.urls')),                    # users 앱
-    path('ingredients/', include('ingredients.urls')),  # ingredients 앱
-    path('recipes/', include('recipes.urls')),          # recipes 앱
-    path('logs/', include('logs.urls')),                # logs 앱
 ]
 
 # 미디어 파일 서빙 (개발 환경)
