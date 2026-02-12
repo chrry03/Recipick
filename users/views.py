cc27de77ff86f14d3c9b09a3cf60f49daa48aed8
chrry03 <ocr925@dgu.ac.kr>
2026-02-12 16:16:52 +0900

    카카오 소셜로그인 시크릿 사용하도록 view수정

--- users/views.py
+++ users/views.py
@@ -366,7 +366,7 @@ def kakao_callback_view(request):
     token_data = {
         "grant_type": "authorization_code",
         "client_id": settings.KAKAO_CLIENT_ID,
-        # client_secret을 제거했습니다.
+        "client_secret": settings.KAKAO_CLIENT_SECRET,
         "redirect_uri": settings.KAKAO_REDIRECT_URI,
         "code": code,
     }
